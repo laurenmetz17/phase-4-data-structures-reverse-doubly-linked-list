1b76362824e2c49dfc01c15ae74e687314929c33
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-07-10 22:06:22 -0400

    commit tree b87d8e1255118686261daacd9a0fc3e4f889c74a

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,6 +9,21 @@ class LinkedList
   end
 
   def reverse!
-    # your code here
+    temp = nil
+    current = @head
+    count = 0 
+    if @head != @tail
+      while current != nil
+        temp = current.prev_node
+        current.prev_node = current.next_node
+        current.next_node = temp
+        current = current.prev_node
+      end
+
+      if temp != nil
+        @tail = @head
+        @head = temp.prev_node
+      end
+    end
   end
 end
